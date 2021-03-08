fae986eda68cbf2b59440b1182746739bbe49ef1
Tanja Olsson <tanja.olsson@skola.botkyrka.sem>
2021-03-08 15:30:40 +0100

    Prototyp

--- main.lua
+++ main.lua
@@ -1,15 +1,17 @@
 x = 650
 y = 55
+text = ""
 
 love.draw = function ()
     love.graphics.circle("fill", 100, 100, 60)
     love.graphics.circle("fill", 300, 100, 60)
     love.graphics.circle("fill", 500, 100, 60)
     love.graphics.rectangle("fill", x, y, 100, 100)
+    love.graphics.print(text, 0, 0)
 end
 
 function love.mousepressed(x, y, button)
-    if button == 'l' then
+    if button == 1 then
        text = "CORRECT!"
     end
  end
